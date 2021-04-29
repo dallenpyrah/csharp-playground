8fe4a17999db4b8da69a59935d0209af2162c0d3
Dallen Pyrah <dallenpyrah@gmail.com>
2021-04-29 16:25:05 -0600

    Repo layer done

--- Repositories/CarsRepository.cs
+++ Repositories/CarsRepository.cs
@@ -1,34 +1,85 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 using csharp_playground.Models;
+using Dapper;
 
 namespace csharp_playground.Repositories
 {
     public class CarsRepository
     {
+
+        private readonly IDbConnection _db;
+
+        public CarsRepository(IDbConnection db)
+        {
+            _db = db;
+        }
+
         internal IEnumerable<Car> GetAllCars()
         {
-            throw new NotImplementedException();
+            string sql = @"
+            SELECT
+            car.*,
+            profile.*
+            FROM cars car
+            JOIN profiles profile ON car.creatorId = profile.id;";
+            return _db.Query<Car, Profile, Car>(sql, (car, profile) =>
+            {
+                car.Creator = profile;
+                return car;
+            }, splitOn: "id");
         }
 
         internal Car GetOneCar(int id)
         {
-            throw new NotImplementedException();
+            string sql = @"
+            SELECT
+            car.*,
+            profile.*
+            FROM cars car 
+            JOIN profiles profile ON car.creatorId = profile.id
+            WHERE car.id = @id;";
+            return _db.Query<Car, Profile, Car>(sql, (c, p) =>
+            {
+                c.Creator = p;
+                return c;
+            }, new { id }, splitOn: "id").FirstOrDefault();
         }
 
         internal Car CreateOneCar(Car newCar)
         {
-            throw new NotImplementedException();
+            string sql = @"
+            INSERT INTO cars
+            (brand, model, color, price, topSpeed, year, creatorId)
+            VAULES
+            (@Brand, @Model, @Color, @Price, @TopSpeed, @Year, @CreatorId);
+            SELECT LAST_INSERT_ID();";
+            int id = _db.ExecuteScalar<int>(sql, newCar);
+            newCar.Id = id;
+            return newCar;
         }
 
         internal Car EditOneCar(Car editedCar)
         {
-            throw new NotImplementedException();
+            string sql = @"
+            UPDATE cars
+                SET
+                brand = @Brand, 
+                model = @Model,
+                color = @Color,
+                price = @Price,
+                topSpeed = @TopSpeed,
+                year = @Year
+                WHERE id = @id;";
+            return _db.QueryFirstOrDefault<Car>(sql, editedCar);
         }
 
         internal Car DeleteOneCar(int id)
         {
-            throw new NotImplementedException();
+            string sql = "DELETE FROM cars WHERE id = @id LIMIT 1;";
+            return _db.QueryFirstOrDefault<Car>(sql, new { id });
         }
     }
 }
--- Setup.sql
+++ Setup.sql
@@ -1,1 +1,28 @@
-USING csharpplayground
+USE csharpplayground;
+
+-- CREATE TABLE profiles
+-- (
+--   id VARCHAR(255) NOT NULL,
+--   email VARCHAR(255) NOT NULL,
+--   name VARCHAR(255),
+--   picture VARCHAR(255),
+--   PRIMARY KEY (id)
+-- );
+
+CREATE TABLE cars
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    creatorId VARCHAR(255) NOT NULL,
+    brand VARCHAR(255) NOT NULL,
+    model VARCHAR(255) NOT NULL,
+    color VARCHAR(255) NOT NULL,
+    price INT,
+    topSpeed INT,
+    year INT NOT NULL,
+
+    PRIMARY KEY (id),
+
+    FOREIGN KEY (creatorId)
+    REFERENCES profiles (id)
+    ON DELETE CASCADE
+);
