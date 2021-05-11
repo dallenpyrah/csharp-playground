13a2974f5ba59904b8282f35ebee0f79efd15495
Dallen Pyrah <dallenpyrah@gmail.com>
2021-05-11 11:26:54 -0600

    Houses service done

---
+++ Repositories/HousesRepository.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using csharp_playground.Models;
+
+namespace csharp_playground.Repositories
+{
+    public class HousesRepository
+    {
+        internal IEnumerable<House> getAllHouses()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal House getHouseById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal House createHouse(House newHouse)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal House editHouse(House editHouse)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal House deleteHouse(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Services/HousesService.cs
+++ Services/HousesService.cs
@@ -1,33 +1,60 @@
 using System;
+using System.Collections.Generic;
 using csharp_playground.Models;
+using csharp_playground.Repositories;
 
 namespace csharp_playground.Services
 {
     public class HousesService
     {
-        internal object getAllHouses()
+        private readonly HousesRepository _hrepo;
+        internal IEnumerable<House> getAllHouses()
         {
-            throw new NotImplementedException();
+            return _hrepo.getAllHouses();
         }
 
-        internal object getHouseById(int id)
+        internal House getHouseById(int id)
         {
-            throw new NotImplementedException();
+            House house = _hrepo.getHouseById(id);
+            if (house == null)
+            {
+                throw new SystemException("Invalid Id: No house found");
+            }
+            return house;
         }
 
-        internal object createHouse(House newHouse)
+        internal House createHouse(House newHouse)
         {
-            throw new NotImplementedException();
+            return _hrepo.createHouse(newHouse);
         }
 
-        internal object editHouse(House editHouse)
+        internal House editHouse(House editHouse)
         {
-            throw new NotImplementedException();
+            House current = getHouseById(editHouse.Id);
+            if (current.CreatorId != editHouse.CreatorId)
+            {
+                throw new SystemException("You are not the creator you can not edit this.");
+            }
+            editHouse.Bathrooms = editHouse.Bathrooms > 0 ? editHouse.Bathrooms : current.Bathrooms;
+            editHouse.Bedrooms = editHouse.Bedrooms > 0 ? editHouse.Bathrooms : current.Bedrooms;
+            editHouse.DateAvaliable = editHouse.DateAvaliable != null ? editHouse.DateAvaliable : current.DateAvaliable;
+            editHouse.GuestLimit = editHouse.GuestLimit > 0 ? editHouse.GuestLimit : current.GuestLimit;
+            editHouse.Image = editHouse.Image != null ? editHouse.Image : current.Image;
+            editHouse.Location = editHouse.Location != null ? editHouse.Location : current.Location;
+            editHouse.PricePerNight = editHouse.PricePerNight > 0 ? editHouse.PricePerNight : current.PricePerNight;
+            editHouse.Reviews = editHouse.Reviews >= 0 ? editHouse.Reviews : current.Reviews;
+            editHouse.SqaureFeet = editHouse.SqaureFeet > 0 ? editHouse.SqaureFeet : current.SqaureFeet;
+            editHouse.SuperHost = editHouse.SuperHost != current.SuperHost ? editHouse.SuperHost : current.SuperHost; 
+            return _hrepo.editHouse(editHouse);
         }
 
-        internal object deleteHouse(int id1, string id2)
+        internal House deleteHouse(int id, string userId)
         {
-            throw new NotImplementedException();
+            House current = getHouseById(id);
+            if(current.CreatorId != userId){
+                throw new SystemException("You are not the creator you can not delete this.");
+            }
+            return _hrepo.deleteHouse(id);
         }
     }
 }
